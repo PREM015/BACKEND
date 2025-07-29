15b722c55cc30b3177af3f00d10c2e84363d20b7
PREM015 <152625984+PREM015@users.noreply.github.com>
2025-07-29 12:05:11 +0530

    update db connect

--- nextjs-backend/src/lib/dbconnect.ts
+++ nextjs-backend/src/lib/dbconnect.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-
 type connectionobject = {
   isconnected?: number;
 };
@@ -8,24 +7,21 @@ type connectionobject = {
 const connection: connectionobject = {};
 
 async function dbconnect(): Promise<void> {
-
-
-    // check if we had done the connection before this or not  or its already connected 
-    if (connection.isconnected) {
-        console.log("already connect ");
-        return ;
-        
-    }
-    try {
-        // try to connect to database
-        const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+  // check if we had done the connection before this or not  or its already connected
+  if (connection.isconnected) {
+    console.log("already connect ");
+    return;
+  }
+  try {
+    // try to connect to database
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
 
     connection.isconnected = db.connections[0].readyState;
 
-    console.log('Database connected successfully');
-    } catch (error) {
-        console.error("database connection falied : " ,error)
-          process.exit(1);
-    }
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("database connection falied : ", error);
+    process.exit(1);
+  }
 }
-export default dbConnect;
+export default dbconnect;
